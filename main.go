ecff8494efb9bb6f6b020dc8897bead5737c8713
scratazon <106277730+scratazon@users.noreply.github.com>
2022-10-16 13:36:13 -0500

    Clean up some cruft

--- main.go
+++ main.go
@@ -21,10 +21,10 @@ type E6Generated struct {
 		File struct {
 			Url string
 		}
-		//Tags struct {
-		//	General []string
-		//	Species []string
-		//}
+		Tags struct {
+			General []string
+			Species []string
+		}
 	}
 }
 
@@ -38,10 +38,6 @@ func ec(e error) {
 	}
 }
 
-//func err(w http.ResponseWriter, req *http.Request) {
-//	tpl.ExecuteTemplate(w, ERROR, nil)
-//}
-
 type le struct {
 	Result string
 }
@@ -54,12 +50,10 @@ func docRoot(w http.ResponseWriter, req *http.Request) {
 	err := req.ParseForm()
 	ec(err)
 
-	//jsonURL := "https://e621.net/posts.json"
 	ua := "Never gonna give you up / I don't know how to use JSON sorry"
 
 	client := &http.Client{}
 
-	//I	rand.Seed(time.Now().UnixNano())
 	rand.Seed(time.Now().UnixNano())
 	var bj E6Generated
 	//	var boobTags = []string{
@@ -74,7 +68,6 @@ func docRoot(w http.ResponseWriter, req *http.Request) {
 
 	boobURL := "https://e621.net/posts.json?tags=squirrel+paws+boobie" // + randomBoobs
 
-	//tags := "?tags=shark+intersex+gaping_anus"
 	req, e := http.NewRequest("GET", boobURL, nil)
 	ec(e)
 
@@ -91,29 +84,10 @@ func docRoot(w http.ResponseWriter, req *http.Request) {
 	l.Result = bj.Posts[rand.Intn(len(bj.Posts))].File.Url
 	tpl.ExecuteTemplate(w, IndexHtml, l)
 
-	//in, e := io.ReadAll(r.Body)
-	//ec(e)
-
-	//json.Unmarshal(in, &bj)
-
-	//for i := 0; i <= 75; i++ {
-	//	time.Sleep(1000 * time.Millisecond)
-	//	if bj.Posts[i].Sample.Url == "" {
-	//		fmt.Printf("Post %d: Very Illegal Post\n", i)
-	//	}
-	//	for _, v := range bj.Posts[i].Tags.Species {
-	//		fmt.Print(v, " ")
-	//	}
-	//	for _, v := range bj.Posts[i].Tags.General {
-	//		fmt.Print(v, " ")
-	//	}
-	//	time.Sleep(1000 * time.Millisecond)
-	//}
 }
 
 func main() {
 	http.HandleFunc("/", docRoot)
-	//http.HandleFunc("/err", err)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	log.Println("Server starting...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
